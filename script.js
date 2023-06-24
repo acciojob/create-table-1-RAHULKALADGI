8446d73c0658c8f8a7740e19637a419c97c90399
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-24 15:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const table = document.getElementById("sampleTable");
+	var new_row = table.insertRow(0);
+	var first = new_row.insertCell(0);
+	first.innerText = "New Cell1";
+	var second = new_row.insertCell(1);
+	second.innerText = "New Cell2";
 }
